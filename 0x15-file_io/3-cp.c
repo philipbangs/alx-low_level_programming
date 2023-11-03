8fdb9ba0422145522644eb974634743178c6b167
philipbangs <philipbangura69@gmail.com>
2023-11-03 16:42:43 -0700

    updating this file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdib.h>
 
-
-
 /**
  * main - function copie the content of one file to the other
  * @argument: The argument count
@@ -12,36 +10,36 @@
  * Return: 1 on success, exit otherwise
  */
 
-void check_IO_stat(int stat, int fd, char *filename, char mode);
+    char *create_buffer(char *file);
+    void close_fill(int py);
 
-int main(int arg, cahar *argnt[])
+    char *create_buffer(char *filled)
+    {
+	    char *buffer;
+	    buffer = malloc(sizeof(char)* 1024);
+	    if (buffer == NULL)
+	    {
+		    dprintf(STDER_FILENO, "error: can not writ %s\n", filled);
+		    exit(99);
+	    }
+	    return (buffer);
+    }
+
+	/**
+	 * close_file - close file disc
+	 * @fd: file descriptor closed
+	 */
+	
+	void close_file(int py)
 {
-    int src, dest, n_need = 1024, wrote, close_src, close_dest;
-    unsigned int mode = s_IRUSR | s_IRGRF | s_INGRT | s_IROTH;
-    char buffer(1024);
+	int by;
+	by = close(py);
 
-    if (arg != 3)
-    {
-        dprintf(STDERR_FILENO, "%S", "Usage: cp file_from file_to \n");
-        exit(97);
-    }
-    src = open(arg[1], o_RDONLY);
-    check_IO_stat(src, -1, arg[1], 'o');
-    dest = open(arg[2], o_WRONLY | o_CREAT | o_TRUNC, mode);
-    check_IO_stat(dest, -1, arg[2], 'W');
-    while (n_read == 1024)
-    {
-        n_read = read(src, buffer, sizeof(buffer));
-        if (nread == -1)
-            check_IO_stat(-1, -1, arg[1], 'o');
-        wrote = write(dest, buffer, n_read);
-        if (wrote == -1)
-            check_IO_stat(-1, -1, arg[2], 'W')
-    }
-    close_src = close(src);
-    check_IO_stat(close_src, src, NULL, 'c');
-    check_IO_stat(close_dest, dest, NULL, 'c');
-    return 0;
+	if(by == -1)
+	{
+		dprintf(STDER_FILENO, "Error: can not close py %d\n", py);
+		exit(100);
+	}
 }
 
 /**
